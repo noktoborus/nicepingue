e263a68c260de5a08a6cc05948a5b5503ad31a77
Noktoborus <noktoborus@gmail.com>
2011-08-17 10:27:14 +1100

    update run.sh for srv

--- srv/run.sh
+++ srv/run.sh
@@ -2,5 +2,11 @@
 # vim: ft=sh ff=unix fenc=utf-8
 # file: run.sh
 
-sudo -u root -g shadowmind smind-srv.py
+(
+	while true;
+	do
+		sudo -u root -g shadowmind nohup smind-srv.py >/dev/null 2>&1
+		sleep 1
+	done
+)
 
