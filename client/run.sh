e5d34b59fc0c60513eeacf3669e6902c08dbc616
Noktoborus <noktoborus@gmail.com>
2011-09-08 16:31:12 +1100

    fix locales

--- client/run.sh
+++ client/run.sh
@@ -3,6 +3,9 @@
 # file: run.sh
 
 PATH="/home/fix:${PATH}"
+export LC_ALL=C
+export LANG=C
+export LANGUAGE=C
 modprobe i2c-dev
 lshwp.py | ssh -o "NumberOfPasswordPrompts 0" -o "ConnectTimeout 3" -o "ServerAliveInterval 1" -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i /home/fix/shadowmind.rsa shadowmind@nicepingue.vladlink.ru 'nc' '-U' 'socket'
 
